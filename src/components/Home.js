d4f5323c6c65bd405c10c4c6039f51cac3806ff2
OUNI Hamza <hamzaouni@gmail.com>
2023-01-04 11:38:03 +0100

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Home Page!</h1>
+      <h2 style={{ textAlign: "left" }}>Qu'est-ce que Développeur web ?</h2>
+      <p style={{ textDecoration: "justify" }}>
+        Un développeur web est un informaticien spécialisé dans la programmation
+        ou expressément impliqué dans le développement des applications du web,
+        ou des applications qui sont exécutées à partir d'un serveur web sur un
+        navigateur web et qui utilisent le protocole HTTP comme vecteur de
+        transmission de l'information.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
